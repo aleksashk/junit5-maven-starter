f69d78a87cd6fce3c0961bbfb80bd07c87473436
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-21 18:35:12 +0300

    TDD - test driven development

--- src/main/java/by/flameksandr/dto/User.java
+++ src/main/java/by/flameksandr/dto/User.java
@@ -1,4 +1,12 @@
 package by.flameksandr.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class User {
+    Integer id;
+    String username;
+    String password;
 }
--- src/test/java/by/flameksandr/junit/service/UserServiceTest.java
+++ src/test/java/by/flameksandr/junit/service/UserServiceTest.java
@@ -4,11 +4,15 @@ import by.flameksandr.dto.User;
 import by.flameksandr.service.UserService;
 import org.junit.jupiter.api.*;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceTest {
+    private static final User ANDREY = new User(1, "Andrey", "andrey");
+    private static final User ALEKS = new User(2, "Aleks", "password");
     private UserService userService;
 
     @BeforeAll
@@ -33,11 +37,34 @@ public class UserServiceTest {
     @Test
     void usersSizeIfUserAdded() {
         System.out.println("Test 2: " + this);
-        userService.add(new User());
-        userService.add(new User());
+        userService.add(ANDREY);
+        userService.add(ALEKS);
         var users = userService.getAll();
 
         assertEquals(2, users.size());
+    }
+
+    @Test
+    void loginSuccessIfUserExists() {
+        userService.add(ALEKS);
+        Optional<User> maybeUser = userService.login(ALEKS.getUsername(), ALEKS.getPassword());
+
+        assertTrue(maybeUser.isPresent());
+        maybeUser.ifPresent(user -> assertEquals(ALEKS, user));
+    }
+
+    @Test
+    void loginFailIfPasswordIsNotCorrect() {
+        userService.add(ANDREY);
+        Optional<User> maybeUser = userService.login(ANDREY.getUsername(), "password");
+        assertTrue(maybeUser.isEmpty());
+    }
+
+    @Test
+    void loginFailIfUSerDoesNotExist() {
+        userService.add(ANDREY);
+        Optional<User> maybeUser = userService.login("Boris", ANDREY.getPassword());
+        assertTrue(maybeUser.isEmpty());
     }
 
     @AfterEach
